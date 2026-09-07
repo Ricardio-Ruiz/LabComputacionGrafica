3ddcc55b085788d273a437cb547b6c8d638245c8
Ricardio-Ruiz <ricardio_ruiz618@comunidad.unam.mx>
2026-09-07 17:50:34 -0600

    Update Main_Modelado.cpp
     correción patas

--- Practica1/Main_Modelado.cpp
+++ Practica1/Main_Modelado.cpp
@@ -221,7 +221,7 @@ int main() {
 		// Pata 1
 		model = glm::mat4(1.0f);
 		model = glm::scale(model, glm::vec3(0.2f, 1.5f, 0.2f));				//Ancho, grosor y profundidad 
-		model = glm::translate(model, glm::vec3(6.5f, -0.47f, 4.0f));			//pocisión en ejes x, y, z
+		model = glm::translate(model, glm::vec3(6.5f, -0.46f, 4.0f));			//pocisión en ejes x, y, z
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 	
@@ -229,7 +229,7 @@ int main() {
 		// Pata 2
 		model = glm::mat4(1.0f);
 		model = glm::scale(model, glm::vec3(0.2f, 1.5f, 0.2f));				
-		model = glm::translate(model, glm::vec3(6.5f, -0.47f, -4.0f));			
+		model = glm::translate(model, glm::vec3(6.5f, -0.46f, -4.0f));			
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
@@ -237,7 +237,7 @@ int main() {
 		// Pata 3
 		model = glm::mat4(1.0f);
 		model = glm::scale(model, glm::vec3(0.2f, 1.5f, 0.2f));
-		model = glm::translate(model, glm::vec3(-6.0f, -0.47f, 4.0f));
+		model = glm::translate(model, glm::vec3(-6.0f, -0.46f, 4.0f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
@@ -245,7 +245,7 @@ int main() {
 		// Pata 4
 		model = glm::mat4(1.0f);
 		model = glm::scale(model, glm::vec3(0.2f, 1.5f, 0.2f));
-		model = glm::translate(model, glm::vec3(-6.0f, -0.47f, -4.0f));
+		model = glm::translate(model, glm::vec3(-6.0f, -0.46f, -4.0f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		
